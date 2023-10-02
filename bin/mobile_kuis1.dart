a0c040caeb456160fc76adec83b3bc3ce1f620b8
Ahmadtito17 <ahmadtitohendrawan@gmail.com>
2023-10-02 14:26:09 +0700

    Kuis 1 - Pemrograman Mobile

---
+++ bin/mobile_kuis1.dart
@@ -0,0 +1,76 @@
+import 'package:mobile_kuis1/mobile_kuis1.dart' as mobile_kuis1;
+
+void main() {
+  // //Tugas 1
+  // print('Hello world');
+
+  // //Tugas 2
+  // String name = 'joko anton jodi';
+
+  // print(name);
+  // print(name);
+  // print(name);
+
+  // //Tugas 3
+  // String firstName = 'joko';
+  // final lastName = 'anton jodi';
+
+  // print(firstName);
+  // print(lastName);
+
+  //Tugas 4
+  var array1 = [1, 2, 3];
+  List<int> array2 = [4, 5, 6];
+
+  array1[0] = 100;
+  array2[0] = 100;
+
+  print(array1);
+  print(array2);
+
+  // //Tugas 4
+  // int number1 = 100;
+  // double number2 = 100.5;
+
+  // print(number1);
+  // print(number2);
+
+  // //Tugas 5
+  // num number = 10;
+  // print(number);
+
+  // number = 10.5;
+  // print(number);
+
+  // //Tugas 6
+  // String firstName = 'joko';
+  // String LastName = 'anton jodi';
+
+  // var fullName = '$firstName ${LastName}';
+
+  // print(fullName);
+
+  // //Tugas 7
+  // var text = 'ini \'OPTIMAL\' \$sekali';
+  // print(text);
+
+  // //Tugas 8
+  // var name1 = firstName + lastName;
+  // var name2 = 'joko' 'anton' 'jodi';
+  // print(name1);
+  // print(name2);
+
+  // //Tugas 9
+  // var name1 = firstName + " " + lastName;
+  // var name2 = 'joko' 'anton' 'jodi';
+  // print(name1);
+  // print(name2);
+
+  // //Tugas 10
+  // var kalimat_panjang = ''''
+  // ini 
+  // saya punya
+  // proyek dart
+  // banyak ''';
+  // print(kalimat_panjang);
+}
